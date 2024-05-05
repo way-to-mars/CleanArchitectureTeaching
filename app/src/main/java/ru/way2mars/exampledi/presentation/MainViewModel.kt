fff6962e1cd0a9b820cbc6580ffeed40918a03bc
Sergio Wallace <way2mars@hotmail.com>
2024-05-05 20:22:47 +0700

    Add a few KDocs

--- app/src/main/java/ru/way2mars/exampledi/presentation/MainViewModel.kt
+++ app/src/main/java/ru/way2mars/exampledi/presentation/MainViewModel.kt
@@ -7,6 +7,14 @@ import ru.way2mars.exampledi.domain.models.SaveDataObject
 import ru.way2mars.exampledi.domain.usecase.GetDataUseCase
 import ru.way2mars.exampledi.domain.usecase.SaveDataUseCase
 
+/**
+ * Реализация вью-модели для *MainActivity*
+ *
+ * @constructor В конструктор передаются юс-кейсы.
+ *
+ * @param getDataUseCase юс-кейс для получения данных [GetDataUseCase].
+ * @param saveDataUseCase юс-кейс для сохранения данных [SaveDataUseCase].
+ */
 class MainViewModel(
     private val getDataUseCase: GetDataUseCase,
     private val saveDataUseCase: SaveDataUseCase,
@@ -20,7 +28,13 @@ class MainViewModel(
         super.onCleared()
     }
 
-
+    /**
+     * Пытается сохранить [text] с помощью [saveDataUseCase].execute().
+     *
+     * Сообщает об успешности сохранения в объект [messageLiveMutable] в виде:
+     *
+     * "Save result: Ok" или "Save result: Failed"
+     */
     fun save(text: String) {
         val result: Boolean = saveDataUseCase.execute(
             SaveDataObject(
@@ -31,6 +45,11 @@ class MainViewModel(
         messageLiveMutable.value = "Save result: ${if (result) "Ok" else "Failed"}"
     }
 
+    /**
+     * Получает объект [DataObject][ru.way2mars.exampledi.domain.models.DataObject] с помощью [getDataUseCase].execute().
+     *
+     * Передает поле message объекта [DataObject][ru.way2mars.exampledi.domain.models.DataObject] в объект [messageLiveMutable]
+     */
     fun load() {
         val dataObject = getDataUseCase.execute()
         messageLiveMutable.value = dataObject.message
